ba3079e7fef5b841a039bcc52f8f493927208408
ZahraCassimjee <231131@virtualwindow.co.za>
2023-10-24 11:28:28 +0200

    merge

--- scripts/index.js
+++ scripts/index.js
@@ -5,12 +5,12 @@ let SiUsername = document.getElementById("SiUsername").value;
 let SiPassword = document.getElementById("SiPassword").value;
 let SiPasswordC = document.getElementById("SiPasswordC").value;
 
-// localStorage.setItem('ls_SiUsername', SiUsername)
-// localStorage.setItem('ls_SiPassword', SiPassword)
-// localStorage.setItem('ls_SiPasswordC', SiPasswordC)
+localStorage.setItem('ls_SiUsername', SiUsername)
+localStorage.setItem('ls_SiPassword', SiPassword)
+localStorage.setItem('ls_SiPasswordC', SiPasswordC)
 
 
-// }
+}
 
 function signInInfo(event) {
     event.preventDefault();
