f4b5be87bd052f0e0e7a54a28b5b2acc59d225dc
Dhiali <231299@virtualwindow.co.za>
2023-10-27 11:21:50 +0200

    dee

--- scripts/im.js
+++ scripts/im.js
@@ -1,6 +1,6 @@
 const urlParams = new URLSearchParams(window.location.search);
         const movieId = urlParams.get("id");
-        fetch(`https://api.themoviedb.org/3/account/36d3716b309f66d8de33fabd0f6aa6c9${movieId}`)
+        fetch(`https://api.themoviedb.org/3/account/36d3716b309f66d8de33fabd0f6aa6c9${movie-info}`)
             .then(response => response.json())
             .then(data => {
                 const movieInfo = document.getElementById("movie-info");
@@ -13,8 +13,6 @@ const urlParams = new URLSearchParams(window.location.search);
                     <p>Actors: ${data.actors}</p>
                     <p>Writers: ${data.writer}</p>
                  
-                 
-                
                     
                 `;
             })
@@ -22,3 +20,23 @@ const urlParams = new URLSearchParams(window.location.search);
 
 
             
+
+            document.addEventListener("DOMContentLoaded", function () {
+                const apiKey = 'https://api.themoviedb.org/3/account/36d3716b309f66d8de33fabd0f6aa6c9';
+                const movieId = 'movie-info';
+                
+
+                fetch(`https://api.themoviedb.org/3/account/36d3716b309f66d8de33fabd0f6aa6c9/${movie-info}?api_key=${36d3716b309f66d8de33fabd0f6aa6c9}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        const posterURL = data.poster_path;
+                        const posterElement = document.getElementById('movie-poster');
+                        
+                        if (posterURL) {
+                            posterElement.src = `'https://image.tmdb.org/t/p/w500/'${posterURL}`; 
+                        } else {
+                            posterElement.src = '/assets/movie.jpg'; 
+                        }
+                    })
+                    .catch(error => console.error('Error fetching movie poster:', error));
+            });
