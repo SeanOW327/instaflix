7b2b0ce5fb8ca6c500163dcbb632ab90b23ab510
ZahraCassimjee <231131@virtualwindow.co.za>
2023-11-06 20:40:52 +0200

    SIGN UP

--- scripts/index.js
+++ scripts/index.js
@@ -23,15 +23,7 @@ const sendData = (usernameVal, lastnameVal, emailVal, passwordVal, sRate, Count)
        
         localStorage.setItem('userData', JSON.stringify(userData));
 
-        // swal({
-        //     title: "Success",
-        //     text: "Hello " + usernameVal + ", you are registered!",
-        //     icon: "success",
-        //     button: "Go to Home",
-        // }).then(() => {
-            
-        //     window.location.href = "index.html";
-        // });
+      
     }
 };
 
@@ -127,10 +119,10 @@ function setSuccessMsg(input) {
 }
 
 // ALERT TO REDIRECT
-document.getElementById("form").onsubmit = function (e) {
-    e.preventDefault(); 
+// document.getElementById("form").onsubmit = function (e) {
+//     e.preventDefault(); 
 
-       alert ("Hi "+document.getElementById('username').value + ", You are now registered, welcome to Instastream")
-    // Redirect to the home page (replace 'home.html' with your actual home page URL)
-    window.location.href = "/index.html";
-};
+//     //    alert ("Hi "+document.getElementById('username').value + ", You are now registered, welcome to Instastream")
+    
+//     window.location.href = "/index.html";
+// };
